4f6f2fb0fa2bfbb26fc7f939ff0dd20a5062bbd0
Subhra <subhrajyotisahoo2001@gmail.com>
2023-11-28 11:05:40 +0530

    2nd commit multiplication

---
+++ Demo2/src/com/test/Multiplication.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	
+	public static void main(String[] args) {
+	
+				Scanner s=new Scanner(System.in);
+				
+				System.out.println("Enter the data a: ");
+				int a=s.nextInt();
+				System.out.println("Enter the data b: ");
+				int b=s.nextInt();
+				int c=a*b;
+				System.out.println("c:"+c);
+			
+
+		}
+	}
+
+
